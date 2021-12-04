9d1b38d418f52ed802ccbe1bba19e94808a84dbf
devdbicanic <dbicanicdev@gmail.com>
2021-12-04 10:50:16 -0600

    Completed part 2 of day 1

--- day1/day_1.rb
+++ day1/day_1.rb
@@ -1,13 +1,29 @@
 require_relative 'day_1_inputs'
 
-def run
+def run(array = input_array)
   counter = 0
 
-  Inputs.defaults.each_with_index do |input, index|
-    counter += 1 if index != 0 && input > Inputs.defaults[index - 1]
+  array.each_with_index do |input, index|
+    counter += 1 if index != 0 && input > array[index - 1]
   end
 
-  puts counter
+  counter
 end
 
-run
+def sum_arrays
+  return @sum_arrays if defined? @sum_arrays
+  @sum_arrays = begin
+    input_array.each_with_index.map do |input, index|
+      number_array = input_array[index..(index + 2)]
+      next if number_array.size < 3
+      number_array.inject(0, :+)
+    end.compact
+  end
+end
+
+def input_array
+  Inputs.defaults
+end
+
+puts "Day 1 part 1 solution #{run}"
+puts "Day 1 part 2 solution #{run(sum_arrays)}"
